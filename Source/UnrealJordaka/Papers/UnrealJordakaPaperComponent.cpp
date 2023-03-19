e73a3146fdc5dd88683a9c0e87a6761d3fbbd6ae
Takaji Messer <takaji.messer@gmail.com>
2023-03-19 14:01:50 -0700

    Added test pilot idle-front sprites

---
+++ Source/UnrealJordaka/Papers/UnrealJordakaPaperComponent.cpp
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealJordakaPaperComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+UUnrealJordakaPaperComponent::UUnrealJordakaPaperComponent()
+{
+    
+}
+
+UPaperFlipbookComponent* UUnrealJordakaPaperComponent::GetFlipbook(EUnrealJordakaStance stance, EUnrealJordakaDirection direction)
+
+UPaperFlipbookComponent* UUnrealJordakaPaperComponent::CreateFlipbook(const TCHAR* name, const TCHAR* assetPath)
+{
+    UPaperFlipbookComponent* flipbook = CreateDefaultSubobject<UPaperFlipbookComponent>(name);
+    UPaperFlipbook* asset = ConstructorHelpers::FObjectFinder<UPaperFlipbook>(assetPath).Object;
+
+    flipbook->SetFlipbook(asset);
+    flipbook->SetupAttachment(this);
+    flipbook->SetLooping(true);
+    flipbook->SetRelativeLocation(FVector(0.0f, 0.0f, 10.0f));
+    //flipbook->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+    flipbook->SetRelativeScale3D(FVector(4.0f));
+    flipbook->SetWorldRotation(FRotator(0.0f, 0.0f, 0.0f));
+
+    return flipbook;
+}
---
+++ Source/UnrealJordaka/Papers/UnrealJordakaPaperComponent.h
@@ -0,0 +1,61 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "PaperSprite.h"
+#include "PaperFlipbook.h"
+#include "PaperSpriteComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "UnrealJordakaPaperComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EUnrealJordakaStance : uint8
+{
+    Idle,
+    Walking,
+    Rolling
+};
+
+UENUM(BlueprintType)
+enum class EUnrealJordakaDirection : uint8
+{
+    Forwards,
+    Backwards,
+    Rightwards,
+    Leftwards
+};
+
+UCLASS(Blueprintable)
+class UUnrealJordakaPaperComponent : public USceneComponent
+{
+    GENERATED_BODY()
+
+public:
+    UUnrealJordakaPaperComponent();
+
+    UPaperFlipbookComponent* GetFlipbook(EUnrealJordakaStance stance, EUnrealJordakaDirection direction);
+
+private:
+    UPaperFlipbookComponent* CreateFlipbook(const TCHAR* name, const TCHAR* assetPath);
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* IdleFrontFlipbook;
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* IdleBackFlipbook;
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* IdleRightFlipbook;
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* WalkFrontFlipbook;
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* WalkBackFlipbook;
+
+    UPROPERTY(VisibleDefaultsOnly)
+    UPaperFlipbookComponent* WalkRightFlipbook;
+};
+
